233eb031e28aaf3c4beee119fff7c448c44fa039
Игорь Абдуллин <367039@edu.itmo.ru>
2025-09-22 18:08:41 +0300

    init project

---
+++ src/main/java/se/ifmo/information_security_1/InformationSecurity1Application.java
@@ -0,0 +1,13 @@
+package se.ifmo.information_security_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationSecurity1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformationSecurity1Application.class, args);
+	}
+
+}
---
+++ src/test/java/se/ifmo/information_security_1/InformationSecurity1ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.ifmo.information_security_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationSecurity1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
